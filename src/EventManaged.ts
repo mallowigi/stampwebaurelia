04591abe58c6cb72aeb6b7ae1295d1c427d74d92
mallowigi <heliosaian@gmail.com>
2016-10-20 22:33:30 +0300

    event managed

---
+++ src/EventManaged.ts
@@ -0,0 +1,59 @@
+import {inject} from 'aurelia-framework'
+import {EventAggregator, Subscription} from 'aurelia-event-aggregator';
+
+@inject(EventAggregator)
+export class EventManaged {
+  _subscribers: Array<Subscription> = [];
+
+  constructor (private eventBus: EventAggregator) {
+
+  }
+
+  /**
+   * Add a subscriber to a given event
+   * @param event
+   * @param handler
+   */
+  subscribe(event, handler) {
+    if (!this._subscribers[event]) {
+      this._subscribers[event] = [];
+    }
+
+    this._subscribers[event].push(this.eventBus.subscribe(event, handler));
+  }
+
+  unsubscribe(event) {
+    this._subscribers[event].forEach(sub => sub.dispose);
+  }
+
+  /**
+   * When the element is detached
+   */
+  detached () {
+    this._subscribers.forEach(event => this.unsubscribe);
+  }
+}
+
+/**
+ * List of events
+ */
+export const EventNames = {
+  loadingStarted: Symbol()
+};
+
+/**
+ * List of Storage Keys
+ */
+export const StorageKeys = {
+
+};
+
+/**
+ * KeyCodes
+ */
+export const KeyCodes = {
+  VK_TAB: 9,
+  VK_ENTER: 13,
+  VK_ESCAPE: 27,
+  VK_SHIFT: 16
+};
--- src/resources/elements/loading-indicator.ts
+++ src/resources/elements/loading-indicator.ts
@@ -1,10 +1,11 @@
 import * as nprogress from 'nprogress';
 import {noView, autoinject, bindable} from 'aurelia-framework';
 import {EventAggregator} from 'aurelia-event-aggregator';
+import {EventManaged, EventNames} from '../../EventManaged';
 
 @noView
 @autoinject
-export class LoadingIndicator {
+export class LoadingIndicator extends EventManaged {
   /**
    * Whether it be loading
    * @type {boolean}
@@ -18,6 +19,7 @@ export class LoadingIndicator {
   loadingCount = 0;
 
   constructor(private ea: EventAggregator) {
+    super(ea);
     nprogress.configure({showSpinner: false});
   }
 
@@ -29,7 +31,25 @@ export class LoadingIndicator {
   }
 
   setupSubscriptions() {
+    /**
+     * Everytime a xhr loading has started, increment the loading count
+     */
+    this.subscribe(EventNames.loadingStarted, () => {
+      nprogress.start();
+      this.loadingCount++;
+    });
 
+    /**
+     * Everytime a xhr loading has finished, decrement the loading count
+     */
+    this.subscribe(EventNames.loadingFinished, () => {
+      this.loadingCount--;
+
+      if (!this.loading && this.loadingCount <= 0) {
+        nprogress.done();
+        this.loadingCount = 0;
+      }
+    })
   }
 
   /**
